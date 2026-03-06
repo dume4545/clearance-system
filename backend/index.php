b56fa84feb6430bfd3085153fdaa1f099f699bb0
dume4545 <dume4545@gmail.com>
2026-03-06 19:27:27 +0000

    Update index.php

--- backend/index.php
+++ backend/index.php
@@ -1,4 +1,14 @@
 <?php
+// Handle CORS for all requests including OPTIONS preflight
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization, Accept");
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit;
+}
+
 $path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $path = rtrim($path, '/');
 
